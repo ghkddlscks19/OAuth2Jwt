cea9e5a0bf903fdb03bf5d90e9f2ce7cd5c7bfeb
ghkddlscks19 <ghkddlscks19@naver.com>
2024-08-12 16:34:40 +0900

    유저 정보 DB 저장

--- src/main/java/com/example/oauth2jwt/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/oauth2jwt/service/CustomOAuth2UserService.java
@@ -1,14 +1,26 @@
 package com.example.oauth2jwt.service;
 
 import com.example.oauth2jwt.dto.*;
+import com.example.oauth2jwt.entity.User;
+import com.example.oauth2jwt.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
@@ -27,11 +39,33 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디 값 생성
         String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
-        UserDto userDto = new UserDto();
-        userDto.setUsername(username);
-        userDto.setName(oAuth2Response.getName());
-        userDto.setRole("ROLE_USER");
+        User findUser = userRepository.findByUsername(username);
+        if (findUser == null) {
+            User user = new User();
+            user.setUsername(username);
+            user.setName(oAuth2Response.getName());
+            user.setEmail(oAuth2Response.getEmail());
+            user.setRole("ROLE_USER");
+            userRepository.save(user);
 
-        return new CustomOAuth2user(userDto);
+            UserDto userDto = new UserDto();
+            userDto.setUsername(username);
+            userDto.setName(oAuth2Response.getName());
+            userDto.setRole("ROLE_USER");
+
+            return new CustomOAuth2user(userDto);
+        } else {
+            findUser.setEmail(oAuth2Response.getEmail());
+            findUser.setName(oAuth2Response.getName());
+
+            UserDto userDto = new UserDto();
+            userDto.setUsername(findUser.getUsername());
+            userDto.setName(oAuth2Response.getName());
+            userDto.setRole(findUser.getRole());
+
+            return new CustomOAuth2user(userDto);
+            
+        }
+
     }
 }
